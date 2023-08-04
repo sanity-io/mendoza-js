e9827a82020b00a7bcf6a78d152d1b36ff941a94
Cody Olsen <stipsan@gmail.com>
2023-08-04 16:07:45 +0200

    refactor(tests): replace jest with vite

---
+++ vitest.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitest/config'
+import GithubActionsReporter from 'vitest-github-actions-reporter'
+
+export default defineConfig({
+  test: {
+    reporters: process.env.GITHUB_ACTIONS ? ['default', new GithubActionsReporter()] : 'default',
+  },
+})
