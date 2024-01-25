870250408a8ce38c52dfabfc63b5f8d1f5361035
Cody Olsen <stipsan@gmail.com>
2024-01-25 20:26:38 +0100

    chore(deps): update vitest monorepo to v1 (major)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,10 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
 import {defineConfig} from 'vitest/config'
 import GithubActionsReporter from 'vitest-github-actions-reporter'
 
-import pkg from './package.json'
-
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     reporters: process.env.GITHUB_ACTIONS ? ['default', new GithubActionsReporter()] : 'default',
-    alias: {
-      mendoza: pkg.exports['.'].source,
-    },
   },
 })
